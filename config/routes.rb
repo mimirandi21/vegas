0bae4f99e6d42f5150888e95fd1a841c74b71390
mimirandi21 <mimirandi21@gmail.com>
2021-09-08 10:28:39 -0500

    add home

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,13 @@
 Rails.application.routes.draw do
   resources :transportations
-  resources :trans_categories
-  resources :trans_locations
-  resources :trans_notes
-  resources :trans_likes
-  resources :dest_likes
-  resources :dest_specials
-  resources :dest_recommendations
-  resources :dest_notes
-  resources :dest_locations
-  resources :dest_categories
+
   resources :destinations
-  resources :food_likes
-  resources :food_recommendations
-  resources :food_specials
-  resources :food_notes
-  resources :food_locations
-  resources :food_categories
-  resources :likes
-  resources :notes
-  resources :recommendations
-  resources :specials
-  resources :locations
+ 
+
   resources :categories
   resources :foods
+
+  root '/categories'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
